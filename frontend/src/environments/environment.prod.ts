9aded7c343d75ee6c3dd078d0c5dbe78001b5db5
Ezequiel Tejada <et.github@outlook.com>
2021-11-07 12:52:33 +0000

    Install Angular Fire

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'learning-stripe',
+    appId: '1:561997927179:web:58488d3e601f3b544ec80e',
+    storageBucket: 'learning-stripe.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCwSuiADMUqu2HevJXe_Y2LxwOBF2pryaE',
+    authDomain: 'learning-stripe.firebaseapp.com',
+    messagingSenderId: '561997927179',
+  },
   production: true
 };
